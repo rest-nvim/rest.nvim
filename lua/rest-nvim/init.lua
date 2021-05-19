11fb0548eb152b0296c0808aa8243682a521382d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-05-19 10:46:23 -0400

    rest-nvim/init.lua: follow RFC 2616 syntax

--- lua/rest-nvim/init.lua
+++ lua/rest-nvim/init.lua
@@ -43,7 +43,7 @@ local function parse_url(stmt)
 	local parsed = utils.split(stmt, ' ')
 	return {
 		method = parsed[1],
-        -- Encode URL
+		-- Encode URL
 		url = utils.encode_url(parsed[2]),
 	}
 end
@@ -57,13 +57,13 @@ local function go_to_line(bufnr, line)
 	end)
 end
 
--- get_json retrieves the json lines in the buffer and then returns a json table
--- @param term The term to search, e.g., BODY
+-- get_body retrieves the body lines in the buffer and then returns a raw table
+-- if the body is not a JSON, otherwise, get_body will return a table
 -- @param bufnr Buffer number, a.k.a id
 -- @param stop_line Line to stop searching
 -- @param query_line Line to set cursor position
 -- @param json_body If the body is a JSON formatted POST request, false by default
-local function get_json(term, bufnr, stop_line, query_line, json_body)
+local function get_body(bufnr, stop_line, query_line, json_body)
 	if not json_body then
 		json_body = false
 	end
@@ -71,12 +71,13 @@ local function get_json(term, bufnr, stop_line, query_line, json_body)
 	local start_line = 0
 	local end_line = 0
 
-	start_line = fn.search(term .. ' {', '', stop_line)
+	start_line = fn.search('{', '', stop_line)
 	end_line = fn.search('}', 'n', stop_line)
 
 	if start_line > 0 then
 		local json_string = ''
-		local json_lines =
+		local json_lines = {}
+		json_lines =
 			api.nvim_buf_get_lines(bufnr, start_line, end_line - 1, false)
 
 		for _, v in ipairs(json_lines) do
@@ -89,45 +90,128 @@ local function get_json(term, bufnr, stop_line, query_line, json_body)
 
 	go_to_line(bufnr, query_line)
 
-	if json_body then
+	if json_body and json ~= nil then
 		-- If the body is a JSON request then return it as raw string
 		-- e.g. `-d "{\"foo\":\"bar\"}"`
 		json = utils.tbl_to_str(json)
 	end
+
 	return json
 end
 
--- get_array retrieves the array elements in the desired line and then returns a
--- lua table with its elements
--- @param term The term to search, e.g. AUTH
+-- get_headers retrieves all the found headers and returns a lua table with them
 -- @param bufnr Buffer number, a.k.a id
--- @param stop_line Line to stop searching
 -- @param query_line Line to set cursor position
-local function get_array(term, bufnr, stop_line, query_line)
-	local array = nil
-	local start_line = 0
-	local end_line = 0
+local function get_headers(bufnr, query_line)
+	local headers = {}
+	-- Set stop at end of buffer
+	local stop_line = fn.line('$')
+	-- If we should stop iterating over the buffer lines
+	local break_loops = false
+	-- HTTP methods
+	local http_methods = { 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' }
 
-	start_line = fn.search(term .. ' {', '', stop_line)
-	end_line = fn.search('}', 'n', stop_line)
+	-- Iterate over all buffer lines
+	for line = 1, stop_line do
+		local start_line = fn.search(':', '', stop_line)
+		local end_line = start_line
+		local next_line = fn.getbufline(bufnr, line + 1)
+		if break_loops then
+			break
+		end
 
-	if start_line > 0 then
-		array = {}
-		local array_elements =
-			api.nvim_buf_get_lines(bufnr, start_line, end_line - 1, false)
-		for _, element in ipairs(array_elements) do
-			table.insert(array, element)
+		for _, next_line_content in pairs(next_line) do
+			if string.find(next_line_content, '{') then
+				break_loops = true
+				break
+			else
+				local get_header =
+					api.nvim_buf_get_lines(bufnr, start_line - 1, end_line, false)
+
+				for _, header in ipairs(get_header) do
+					header = utils.split(header, ':')
+					if
+						header[1]:lower() ~= 'accept'
+						and header[1]:lower() ~= 'authorization'
+                        -- If header key doesn't contains double quotes,
+                        -- so we don't get body keys
+						and header[1]:find('"') == nil
+                        -- If header key doesn't contains hashes,
+                        -- so we don't get commented headers
+                        and header[1]:find('^#') == nil
+                        -- If header key doesn't contains HTTP methods,
+                        -- so we don't get the http method/url
+                        and not utils.has_value(http_methods, header[1])
+                    then
+						headers[header[1]:lower()] = header[2]
+					end
+				end
+			end
 		end
 	end
 
 	go_to_line(bufnr, query_line)
-	return array
+	return headers
+end
+
+-- get_accept retrieves the Accept field and returns it as string
+-- @param bufnr Buffer number, a.k.a id
+-- @param query_line Line to set cursor position
+local function get_accept(bufnr, query_line)
+	local accept = ''
+	-- Set stop at end of bufer
+	local stop_line = fn.line('$')
+
+	-- Iterate over all buffer lines
+	for _ = 1, stop_line do
+		-- Case-insensitive search
+		local start_line = fn.search('\\cAccept:', '', stop_line)
+		local end_line = start_line
+		local accept_line =
+			api.nvim_buf_get_lines(bufnr, start_line - 1, end_line, false)
+
+		for _, accept_data in pairs(accept_line) do
+			accept = utils.split(accept_data, ':')[2]
+		end
+	end
+
+	go_to_line(bufnr, query_line)
+	return accept
+end
+
+-- get_auth retrieves the HTTP Authorization and returns a lua table with its values
+-- @param bufnr Buffer number, a.k.a id
+-- @param query_line Line to set cursor position
+local function get_auth(bufnr, query_line)
+	local auth = {}
+	-- Set stop at end of bufer
+	local stop_line = fn.line('$')
+
+	-- Iterate over all buffer lines
+	for _ = 1, stop_line do
+		-- Case-insensitive search
+		local start_line = fn.search('\\cAuthorization:', '', stop_line)
+		local end_line = start_line
+		local auth_line =
+			api.nvim_buf_get_lines(bufnr, start_line - 1, end_line, false)
+
+		for _, auth_data in pairs(auth_line) do
+			-- Split by spaces, e.g. {'Authorization:', 'user:pass'}
+			auth_data = utils.split(auth_data, '%s+')
+			-- {'user', 'pass'}
+			auth = utils.split(auth_data[2], ':')
+		end
+	end
+
+	go_to_line(bufnr, query_line)
+	return auth
 end
 
 -- curl_cmd runs curl with the passed options, gets or creates a new buffer
 -- and then the results are printed to the recently obtained/created buffer
 -- @param opts curl arguments
 local function curl_cmd(opts)
+	Opts = opts
 	local res = curl[opts.method](opts)
 	local res_bufnr = get_or_create_buf()
 	local parsed_url = parse_url(fn.getline('.'))
@@ -152,7 +236,7 @@ local function curl_cmd(opts)
 		false,
 		{ parsed_url.method .. ' ' .. parsed_url.url }
 	)
-	-- Status code and its meaning, e.g. HTTP/1.1 200 OK
+	-- HTTP version, status code and its meaning, e.g. HTTP/1.1 200 OK
 	line_count = api.nvim_buf_line_count(res_bufnr)
 	api.nvim_buf_set_lines(
 		res_bufnr,
@@ -222,41 +306,30 @@ local function run()
 		)
 	next_query = next_query > 1 and next_query or fn.line('$')
 
-	local headers =
-		get_json('HEADERS', bufnr, next_query, last_query_line_number)
+	headers = get_headers(bufnr, last_query_line_number)
 
 	local body = {}
 	-- If the header Content-Type was passed and it's application/json then return
 	-- body as `-d '{"foo":"bar"}'`
 	if
 		headers ~= nil
-		and headers['Content-Type'] ~= nil
-		and string.find(headers['Content-Type'], 'application/json')
+		and headers['content-type'] ~= nil
+		and string.find(headers['content-type'], 'application/json')
 	then
-		body = get_json(
-			'BODY',
-			bufnr,
-			next_query,
-			last_query_line_number,
-			true
-		)
+		body = get_body(bufnr, next_query, last_query_line_number, true)
 	else
-		body = get_json('BODY', bufnr, next_query, last_query_line_number)
+		body = get_body(bufnr, next_query, last_query_line_number)
 	end
 
-	local queries =
-		get_json('QUERIES', bufnr, next_query, last_query_line_number)
-	local form = get_json('FORM', bufnr, next_query, last_query_line_number)
-	local auth =
-		get_array('AUTH', bufnr, next_query, last_query_line_number)
+	local auth = get_auth(bufnr, last_query_line_number)
+	local accept = get_accept(bufnr, last_query_line_number)
 
 	curl_cmd({
 		method = parsed_url.method:lower(),
 		url = parsed_url.url,
-		query = queries,
 		headers = headers,
+		accept = accept,
 		body = body,
-		form = form,
 		auth = auth,
 	})
 
