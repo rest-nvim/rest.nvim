67f86bcd4feca9d33bba57f697dda9be4d2d78c9
Michał Modzelewski <mr-majkel@users.noreply.github.com>
2021-07-21 19:49:08 +0200

    prevent modified flag switch for httpResult (#34)

--- lua/rest-nvim/init.lua
+++ lua/rest-nvim/init.lua
@@ -21,6 +21,8 @@ local function get_or_create_buf()
 	if existing_bufnr ~= -1 then
 		-- Set modifiable
 		vim.api.nvim_buf_set_option(existing_bufnr, 'modifiable', true)
+                -- Prevent modified flag
+		vim.api.nvim_buf_set_option(existing_bufnr, 'buftype', 'nofile')
 		-- Delete buffer content
 		vim.api.nvim_buf_set_lines(
 			existing_bufnr,
@@ -40,6 +42,7 @@ local function get_or_create_buf()
 	local new_bufnr = vim.api.nvim_create_buf(false, 'nomodeline')
 	vim.api.nvim_buf_set_name(new_bufnr, tmp_name)
 	vim.api.nvim_buf_set_option(new_bufnr, 'ft', 'httpResult')
+        vim.api.nvim_buf_set_option(new_bufnr, 'buftype', 'nofile')
 
 	return new_bufnr
 end
