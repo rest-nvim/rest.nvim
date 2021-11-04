713ba63cb1d3be15a7aada3f65a3252c60c59383
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-11-04 16:58:26 -0400

    feat: update highlights query

--- after/queries/http/highlights.scm
+++ after/queries/http/highlights.scm
@@ -1,3 +1,5 @@
+; inherits: json
+
 ; Display errors
 (ERROR) @error
 
@@ -6,28 +8,12 @@
 
 (request
   method: (method) @keyword
-  url: (url) @TSURI)
+  url: (url) @text.uri)
 
 (header
   name: (name) @constant
   value: (value))
 
-; JSON
-(json_pair
-  key: (_) @keyword)
-
-(json_string) @string
-
-(json_number) @number
-
-(json_boolean) @boolean
-
-(json_null) @constant
-
-(json_escape_sequence) @TSStringEscape
-
-(json_body) @keyword
-
 ; rest.nvim Neovim plugin specific features
 (external_body
-  json_file: (json_file) @TSURI) @keyword
+  json_file: (json_file) @text.uri) @keyword
