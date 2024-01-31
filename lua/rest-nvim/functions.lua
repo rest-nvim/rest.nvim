8f7f7a0162ccedfc68d9b6ff05f1feaa3e63237d
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-01-30 21:36:38 -0400

    feat(functions): refactor `nio` code to properly return the request results table, open results buffer

--- lua/rest-nvim/functions.lua
+++ lua/rest-nvim/functions.lua
@@ -11,13 +11,13 @@ local functions = {}
 local nio = require("nio")
 local utils = require("rest-nvim.utils")
 local parser = require("rest-nvim.parser")
+local result = require("rest-nvim.result")
 
 ---Execute or `preview` one or several HTTP requests depending on given `scope`
 ---and return request(s) results in a table that will be used to render results
 ---in a buffer.
 ---@param scope string Defines the request execution scope. Can be: `last`, `cursor` (default) or `document`
 ---@param preview boolean Whether execute the request or just preview the command that is going to be ran. Default is `false`
----@return table Request results (HTTP client output)
 function functions.exec(scope, preview)
   vim.validate({
     scope = { scope, "string" },
@@ -49,18 +49,17 @@ function functions.exec(scope, preview)
   -- TODO: implement `document` scope.
   --
   -- NOTE: The `document` scope may require some parser adjustments
+  local req_results = {}
+
   if scope == "cursor" then
     local req = parser.parse(
       ---@diagnostic disable-next-line param-type-mismatch
       parser.look_behind_until(parser.get_node_at_cursor(), "request")
     )
 
-    local sleep = nio.wrap(function(ms, cb)
-      vim.defer_fn(cb, ms)
-    end, 2)
-    nio.run(function()
-      sleep(10, client.request(req))
-    end)
+    req_results = nio.run(function()
+      return client.request(req)
+    end):wait()
 
     ---Last HTTP request made by the user
     ---@type Request
@@ -72,14 +71,14 @@ function functions.exec(scope, preview)
       ---@diagnostic disable-next-line need-check-nil
       logger:error("Rest run last: A previously made request was not found to be executed again")
     else
-      local sleep = nio.wrap(function(ms, cb)
-        vim.defer_fn(cb, ms)
-      end, 2)
-      nio.run(function()
-        sleep(10, client.request(req))
-      end)
+      req_results = nio.run(function()
+        return client.request(req)
+      end):wait()
     end
   end
+
+  local result_buf = result.get_or_create_buf()
+  result.write_res(result_buf, req_results)
 end
 
 ---Manage the environment file that is currently in use while running requests
