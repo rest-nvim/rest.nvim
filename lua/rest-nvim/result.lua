78bf6acaa4f493f865386e2135ca903b625b5454
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-01-30 23:47:46 -0400

    feat(result): write request result body to results buffer, improve docs and fix some issues

--- lua/rest-nvim/result.lua
+++ lua/rest-nvim/result.lua
@@ -8,6 +8,8 @@
 
 local result = {}
 
+local nio = require("nio")
+
 ---Move the cursor to the desired position in the given buffer
 ---@param bufnr number Buffer handler number
 ---@param row number The desired line
@@ -29,7 +31,7 @@ function result.get_or_create_buf()
   -- Check if the file is already loaded in the buffer
   local existing_buf, bufnr = false, nil
   for _, id in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_get_name(id) == tmp_name then
+    if vim.api.nvim_buf_get_name(id):find(tmp_name) then
       existing_buf = true
       bufnr = id
     end
@@ -115,18 +117,103 @@ end
 
 ---Write request results in the given buffer and display it
 ---@param bufnr number The target buffer
----@
+---@param res table Request results
 function result.write_res(bufnr, res)
+  local logger = _G._rest_nvim.logger
+
   local headers = vim.tbl_filter(function(header)
     if header ~= "" then
       return header
-      ---@diagnostic disable-next-line missing-return
+    ---@diagnostic disable-next-line missing-return
     end
   end, vim.split(res.headers, "\n"))
 
+  -- METHOD http://foo.bar/api/endpoint
   result.write_block(bufnr, { res.method .. " " .. res.url }, true)
+  -- ......
+  -- Content-Type: application/json
+  -- ......
   result.write_block(bufnr, headers, true)
-  -- TODO: write request body
+
+  nio.run(function()
+    ---@type string
+    local res_type
+    for _, header in ipairs(headers) do
+      if header:find("Content%-Type") then
+        local content_type = vim.trim(vim.split(header, ":")[2])
+        -- We need to remove the leading charset if we are getting a JSON
+        res_type = vim.split(content_type, "/")[2]:gsub(";.*", "")
+      end
+    end
+
+    -- Do not try to format binary content
+    if res_type == "octet-stream" then
+      -- That empty value is a newline
+      local body = { "", "Binary answer" }
+      result.write_block(bufnr, body, true)
+    else
+      local formatters = _G._rest_nvim.result.behavior.formatters
+      local filetypes = vim.tbl_keys(formatters)
+
+      -- If there is a formatter for the content type filetype then
+      -- format the request result body, otherwise return it as we got it
+      if vim.tbl_contains(filetypes, res_type) then
+        local fmt = formatters[res_type]
+        if type(fmt) == "function" then
+          local ok, out = pcall(fmt, res.result)
+          if ok and out then
+            res.result = out
+          else
+            ---@diagnostic disable-next-line need-check-nil
+            logger:error("Error calling formatter on response body:\n" .. out)
+          end
+        elseif vim.fn.executable(fmt) == 1 then
+          local stdout = vim.fn.system(fmt, res.result):gsub("\n$", "")
+          -- Check if formatter ran successfully
+          if vim.v.shell_error == 0 then
+            res.result = stdout
+          else
+            ---@diagnostic disable-next-line need-check-nil
+            logger:error(
+              "Error running formatter '" .. fmt .. "' on response body:\n" .. stdout
+            )
+          end
+        end
+      else
+        ---@diagnostic disable-next-line need-check-nil
+        logger:info(
+          "Could not find a formatter for the body type " .. res_type .. " returned in the request, the results will not be formatted"
+        )
+      end
+      local body = vim.split(res.result, "\n")
+      table.insert(body, 1, "")
+      table.insert(body, 2, "#+RES")
+      table.insert(body, "#+END")
+      result.write_block(bufnr, body, true)
+
+      -- add syntax highlights for response
+      vim.api.nvim_buf_call(bufnr, function()
+        local syntax_file = vim.fn.expand(string.format("$VIMRUNTIME/syntax/%s.vim", res_type))
+        if vim.fn.filereadable(syntax_file) == 1 then
+          vim.cmd(string.gsub(
+            [[
+            if exists("b:current_syntax")
+              unlet b:current_syntax
+            endif
+            syn include @%s syntax/%s.vim
+            syn region %sBody matchgroup=Comment start=+\v^#\+RES$+ end=+\v^#\+END$+ contains=@%s
+
+            let b:current_syntax = "httpResult"
+            ]],
+            "%%s",
+            res_type
+          ))
+        end
+      end)
+    end
+  end)
+
+  table.insert(res.statistics, 1, "")
   result.write_block(bufnr, res.statistics, false)
 
   result.display_buf(bufnr)
