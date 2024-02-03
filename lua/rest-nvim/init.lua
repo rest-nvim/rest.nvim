ca5980cbef0f19e4148d524f43e5541442810311
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-03 16:11:41 -0400

    feat(init): also setup the tree-sitter branch on `setup()`

--- lua/rest-nvim/init.lua
+++ lua/rest-nvim/init.lua
@@ -19,6 +19,18 @@ function rest.setup(user_configs)
 
   -- Set up rest.nvim autocommands and commands
   autocmds.setup()
+
+  -- Set up tree-sitter HTTP parser branch
+  -- NOTE: remove this piece of code once rest.nvim v2 has been pushed
+  --       and tree-sitter-http `next` branch has been merged
+  local ok, treesitter_parsers = pcall(require, "nvim-treesitter.parsers")
+  if ok then
+    local parser_config = treesitter_parsers.get_parser_configs()
+
+    parser_config.http = vim.tbl_deep_extend("force", parser_config.http, {
+      install_info = { branch = "next" },
+    })
+  end
 end
 
 return rest
