37e4342f3b3d9ccb4ded12a684a3096f52053deb
Seongmin Lee <boltlessengineer@gmail.com>
2024-08-25 09:17:19 +0000

    chore(nix): update `cats-doc` to forked version

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neorocks.url = "github:nvim-neorocks/neorocks";
     flake-utils.url = "github:numtide/flake-utils";
-    cats-doc.url = "github:mrcjkb/cats-doc";
+    cats-doc.url = "github:boltlessengineer/vimcats?ref=feat/aarch64-linux";
   };
 
   outputs = inputs @ {
